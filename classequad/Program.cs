62b748c5536db22f73b8ff6d63d60c66bc0a1b26
uriel <uriel@LAPTOP-S7K83OQV>
2023-11-29 19:42:56 +0100

    Aggiungere i file di progetto.

---
+++ classequad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classequad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato quadrato1 = new Quadrato();
+            Console.WriteLine("lato del quadrato?");
+            
+            quadrato1.lato = float.Parse(Console.ReadLine());
+
+            quadrato1.AreaQuadrato();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ classequad/quadrato.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classequad
+{
+    internal class Quadrato
+    {
+        public float lato {  get; set; }
+
+        public float AreaQuadrato()
+        {
+            return lato * lato;
+
+        }
+
+
+    }
+}
